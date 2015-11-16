96adbb771abfb52b8ef5c3a6eb3dad20ae64909b
x4d3 <git@xade.eu>
2015-11-16 17:11:47 +0000

    removed sensible information

--- fraction.js
+++ fraction.js
@@ -1,7 +1,16 @@
 Fraction = function() {
 	var Fraction = function(numerator, denominator) {
+		if (denominator == 0) {
+			throw "The denominator must not be zero";
+		}
+		if (denominator < 0) {
+			numerator = -numerator;
+			denominator = -denominator;
+		}
+
 		this.n = numerator;
 		this.d = denominator;
 	};
 	return Fraction;
+
 }();
