a23f6a46a3396bfb666655308b26e380a900b4f2
x4d3 <git@xade.eu>
2015-11-16 18:17:13 +0000

    implemented fraction.js, add test and landing page

--- fraction.js
+++ fraction.js
@@ -1,16 +1,179 @@
-Fraction = function() {
-	var Fraction = function(numerator, denominator) {
-		if (denominator == 0) {
-			throw "The denominator must not be zero";
+Fraction = (function() {
+	var checkType = function(value, type) {
+		if (typeof value !== type) {
+			throw "It should be a " + type + ": " + value;
 		}
-		if (denominator < 0) {
-			numerator = -numerator;
-			denominator = -denominator;
+		return value;
+	};
+	var checkIsNumber = function(value) {
+		return checkType(value, 'number');
+	};
+	var checkIsInt = function(value) {
+		checkIsNumber(value);
+		if (value % 1 !== 0) {
+			throw "It should be an int: " + value;
 		}
+		return value;
+	};
+	
+	var Fraction = function(numerator, denominator, noCheck) {
+		if(!noCheck){
+			checkIsInt(numerator);
+			checkIsInt(denominator);
+			if (denominator === 0) {
+				throw "The denominator must not be zero";
+			}
+			if (denominator < 0) {
+				numerator = -numerator;
+				denominator = -denominator;
+			}
+		}
+		this.numerator = numerator;
+		this.denominator = denominator;
+		this.normalize();
+	};
 
-		this.n = numerator;
-		this.d = denominator;
+	Fraction.prototype.clone = function() {
+		return new Fraction(this.numerator, this.denominator, true);
+	};
+
+	/* pretty-printer, converts fractions into whole numbers and fractions */
+	Fraction.prototype.toString = function() {
+		return this.numerator + "/" + this.denominator;
+
+	};
+
+	/* pretty-printer to support TeX notation (using with MathJax, KaTeX, etc) */
+	Fraction.prototype.toTeX = function(mixed) {
+		var result = '';
+		if ((this.numerator < 0) != (this.denominator < 0))
+			result = '-';
+		var numerator = Math.abs(this.numerator);
+		var denominator = Math.abs(this.denominator);
+
+		if (!mixed) {
+			// We want a simple fraction, without wholepart extracted
+			if (denominator === 1)
+				return result + numerator;
+			else
+				return result + '\\frac{' + numerator + '}{' + denominator + '}';
+		}
+		var wholepart = Math.floor(numerator / denominator);
+		numerator = numerator % denominator;
+		if (wholepart !== 0)
+			result += wholepart;
+		if (numerator !== 0)
+			result += '\\frac{' + numerator + '}{' + denominator + '}';
+		return result.length > 0 ? result : '0';
+	};
+
+	/* destructively rescale the fraction by some integral factor */
+	Fraction.prototype.rescale = function(factor) {
+		this.numerator *= factor;
+		this.denominator *= factor;
+		return this;
+	};
+
+	Fraction.prototype.add = function(b) {
+		var a = this.clone();
+		if (!(b instanceof Fraction)) {
+			throw "must be a Fraction: " + b;
+		}
+		var td = a.denominator;
+		a.rescale(b.denominator);
+		a.numerator += b.numerator * td;
+		return a.normalize();
+	};
+
+	Fraction.prototype.subtract = function(b) {
+		var a = this.clone();
+		if (!(b instanceof Fraction)) {
+			throw "must be a Fraction: " + b;
+		}
+		var td = a.denominator;
+		a.rescale(b.denominator);
+		a.numerator -= b.numerator * td;
+
+		return a.normalize();
+	};
+
+	Fraction.prototype.multiply = function(b) {
+		var a = this.clone();
+		a.numerator *= b.numerator;
+		a.denominator *= b.denominator;
+		return a.normalize();
+	};
+
+	Fraction.prototype.divide = function(b) {
+		if (!(b instanceof Fraction)) {
+			throw "must be a Fraction: " + b;
+		}
+		var a = this.clone();
+		a.numerator *= b.denominator;
+		a.denominator *= b.numerator;
+		return a.normalize();
+	};
+
+	Fraction.prototype.equals = function(b) {
+		if (!(b instanceof Fraction)) {
+			return false;
+		}
+		// fractions that are equal should have equal normalized forms
+		return (a.numerator === b.numerator && a.denominator === b.denominator);
+	};
+
+	Fraction.prototype.compareTo = function(b) {
+		if (!(b instanceof Fraction)) {
+			throw "must be a Fraction: " + b;
+		}
+		if (this === other || this.equals(b)) {
+			return 0;
+		}
+		var first = numerator * other.denominator;
+		var second = other.numerator * denominator;
+		if (first == second) {
+			return 0;
+		} else if (first < second) {
+			return -1;
+		} else {
+			return 1;
+		}
+	};
+
+	Fraction.prototype.normalize = function(){
+		var gcf = Fraction.gcf(this.numerator, this.denominator);
+		this.numerator /= gcf;
+		this.denominator /= gcf;
+		if (this.denominator < 0) {
+			this.numerator *= -1;
+			this.denominator *= -1;
+		}
+		return this;
+	};
+	/* Takes two numbers and returns their greatest common factor. */
+	// Adapted from Ratio.js
+	Fraction.gcf = function(a, b) {
+		if (arguments.length < 2) {
+			return a;
+		}
+		var c;
+		a = Math.abs(a);
+		b = Math.abs(b);
+		/*
+		 * //It seems to be no need in these checks // Same as isNaN() but
+		 * faster if (a !== a || b !== b) { return NaN; } //Same as !isFinite()
+		 * but faster if (a === Infinity || a === -Infinity || b === Infinity ||
+		 * b === -Infinity) { return Infinity; } // Checks if a or b are
+		 * decimals if ((a % 1 !== 0) || (b % 1 !== 0)) { throw new Error("Can
+		 * only operate on integers"); }
+		 */
+
+		while (b) {
+			c = a % b;
+			a = b;
+			b = c;
+		}
+		return a;
 	};
 	return Fraction;
-
-}();
+})();
--- tests/fraction.test.js
+++ tests/fraction.test.js
@@ -1,6 +1,48 @@
 module("Fraction Tests");
+var frac = function(n, d) {
+	return new Fraction(n, d);
+};
 
-test("first test", function() {
-	ok(true, "first test");
-	
+test("create fraction", function() {
+	var fraction = new Fraction(3, 3);
+	equal(fraction.numerator, 1);
+	equal(fraction.denominator, 1);
+
+	throws(function() {
+		var fraction = new Fraction(1);
+	});
+	throws(function() {
+		var fraction = new Fraction(1 / 1);
+	});
+	throws(function() {
+		var fraction = new Fraction("1");
+	});
+	throws(function() {
+		var fraction = new Fraction("1/1");
+	});
+});
+
+test("add fraction", function() {
+	var result = frac(2, 3).add(frac(5, 6)).add(frac(7, 8));
+	var expected = frac(19, 8);
+	ok(expected, result, expected + " != " + result);
+
+});
+
+test("substract fraction", function() {
+	var result = frac(3, 7).subtract(frac(126, 8)).subtract(frac(56, 52));
+	var expected = frac(-5969, 364);
+	ok(expected, result, expected + " != " + result);
+});
+
+test("multiply", function() {
+	var result = frac(3, 7).multiply(frac(126, 8)).multiply(frac(56, 52));
+	var expected = frac(189, 26);
+	ok(expected, result, expected + " != " + result);
+});
+
+test("divide", function() {
+	var result = frac(3, 7).divide(frac(126, 8)).divide(frac(56, 52));
+	var expected = frac(26, 1029);
+	ok(expected, result, expected + " != " + result);
 });
