194160b31d674adc49edef0d959554b03960b8db
x4d3 <git@xade.eu>
2015-11-16 15:29:47 +0000

    re add file

--- deploy-to-page.sh
+++ deploy-to-page.sh
@@ -20,6 +20,7 @@ git config user.email "travis@xade.eu"
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
 git commit -m "Deploy to GitHub Pages"
+
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
